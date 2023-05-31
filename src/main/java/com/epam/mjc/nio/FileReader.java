9d6780ae05e7f8fa7f3e50be796ca00e35f7a7ea
Lapa <lapabig@gmail.com>
2023-05-31 17:03:38 +0600

    Updated

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,27 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        int content;
+        String[] x = new String[8];
+        String text = new String();
+        try (InputStream inputStream = Files.newInputStream(file.toPath())) {
+            do {
+                content = inputStream.read();
+                if (content != -1) {
+                    text = text + (char) content;
+                }
+            } while (content != -1);
+            x = text.split("\r?\n|\r|: ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile(x[1], Integer.valueOf(x[3]), x[5], Long.valueOf(x[7]));
     }
 }
